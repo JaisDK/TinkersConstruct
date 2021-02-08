3b6f7bc68efb8e53121e22761a9d5c4327ae1946
KnightMiner <knightminer4@gmail.com>
2021-02-08 13:21:54 -0500

    Fix crash because Forge does not properly mark their events nullable

--- src/main/java/slimeknights/tconstruct/tools/modifiers/traits/ExperiencedModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/traits/ExperiencedModifier.java
@@ -1,5 +1,6 @@
 package slimeknights.tconstruct.tools.modifiers.traits;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
@@ -33,11 +34,14 @@ public class ExperiencedModifier extends Modifier {
    * @param event  Event
    */
   private void onEntityKill(LivingExperienceDropEvent event) {
-    ToolStack tool = ToolStack.from(event.getAttackingPlayer().getHeldItemMainhand());
-    if (!tool.isBroken()) {
-      int level = tool.getModifierLevel(this);
-      if (level > 0) {
-        event.setDroppedExperience(boost(event.getDroppedExperience(), level));
+    PlayerEntity player = event.getAttackingPlayer();
+    if (player != null) {
+      ToolStack tool = ToolStack.from(player.getHeldItemMainhand());
+      if (!tool.isBroken()) {
+        int level = tool.getModifierLevel(this);
+        if (level > 0) {
+          event.setDroppedExperience(boost(event.getDroppedExperience(), level));
+        }
       }
     }
   }
